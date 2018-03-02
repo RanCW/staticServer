37915ef8d8d18149b549350706bac4ac68c906b3
ranchengwei <rancwdream@sina.com>
2018-03-02 18:55:34 +0800

    change

--- src/app.js
+++ src/app.js
@@ -1,52 +1,78 @@
-const http=require('http')
-const path=require('path')
-const url=require('url')
-const fs=require('fs')
-const {promisify}=require('util')
-const config=require('./config')
-const stat=promisify(fs.stat)
+const http = require('http')
+const path = require('path')
+const url = require('url')
+const fs = require('fs')
+const mime = require('mime')
+const {promisify} = require('util')
+let handlebars = require('handlebars');
+
+const config = require('./config')
+const stat = promisify(fs.stat)
+const readDir = promisify(fs.readdir)
+
+//获取编译模板
+function getTemplet(tmplPath) {
+  let tmpl = fs.readFileSync(path.resolve(__dirname, 'template', tmplPath), 'utf8');
+  return handlebars.compile(tmpl);
+}
 
 
-class Server{
-    constructor(){
-        // this.config=Object.assign({},config);
-        this.config=config;
+class Server {
+  constructor() {
+    this.config = config;
+    this.list=getTemplet('list.html')
+  }
+
+  //启动服务
+  start() {
+    let server = http.createServer();
+    server.on('request', this.request.bind(this))
+    server.listen(this.config.port);
+    console.log(`服务启动成功http://${this.config.host}:${this.config.port}`);
+  }
+
+  async request(req, res) {
+    let pathName = url.parse(req.url).path;
+    let filePath = path.join(this.config.root, pathName);
+    if (filePath.indexOf('favicon.ico') > 0) {
+      this.sendError(req, res, 'not found');
+      return
     }
-    //启动服务
-    start(){
-        let server=http.createServer();
-        server.on('request',this.request.bind(this))
-        server.listen(this.config.port);
-        console.log(`服务启动成功http://${this.config.host}:${this.config.port}`);
+    try {
+      let statObj = await stat(filePath);
+      console.log(statObj);
+      if (statObj.isDirectory()) {
+        console.log(filePath);
+        let directories=readDir(filePath);
+        console.log(directories);
+        res.end('haha')
+
+      } else {
+        this.sendContent(req, res, filePath, statObj);
+      }
+    } catch (err) {
+      this.sendError(req,res,err);
     }
-    async request(req,res){
-        let pathName=url.parse(req.url).path;
-        let filePath=path.join(this.config.root,pathName);
-        if(filePath.indexOf('favicon.ico')>0){
-            this.sendContent(req,res);
-            return
-        }
-        try{
-            let statObj=await stat(filePath);
-            console.log(statObj);
-            if(statObj.isDirectory()){
-                console.log(filePath);
 
-            }else{
-                console.log(`${filePath}是个文件`);
+  }
 
-            }
-        }catch(err) {
-            console.log(err);
+  sendContent(req, res, filePath, statObj) {
+    let fileType= mime.getType(filePath);
+    res.setHeader('Content-Type', `${fileType};charset=UTF-8`);
+    let rs = this.getStream(filePath);//获取文件的可读流
+    rs.pipe(res);
+  }
 
-        }
+  getStream(filePath) {
+    return fs.createReadStream(filePath);
 
-        this.sendContent(req,res);
-    }
-    sendContent(req,res,filePath,statObj){
-        res.setHeader('content-type','text/html;charset=UTF-8')
-        res.end(`访问http://${this.config.host}:${this.config.port}成功`)
-    }
+  }
+
+  sendError(req, res, err) {
+    res.statusCode = 500;
+    res.end(`${err.toString()}`)
+  }
 }
 new Server().start();
-module.exports=Server;
+
+module.exports = Server;
